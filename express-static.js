b919079a1dc61103f114b0984531a03416fd5110
Alvarojavier22 <alvarojavierchagas@hotmail.com>
2023-02-10 21:13:35 -0300

    lento pero seguro

--- express-static.js
+++ express-static.js
@@ -1,10 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  console.log(req.headers)
-  res.send('hello from server')
-})
+app.use('/', express.static(__dirname + "/static"))
 
 app.listen(4000, () => {
   console.log('server listen on http://localhost:4000')
