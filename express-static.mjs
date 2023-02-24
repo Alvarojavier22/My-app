8e4524e329aa9139ff2da3dd562bcd15224724e5
Alvarojavier22 <alvarojavierchagas@hotmail.com>
2023-02-24 17:17:10 -0300

    Por empezar mvc

--- express-static.mjs
+++ express-static.mjs
@@ -1,7 +1,8 @@
-const express = require('express')
+import { Express } from "express";
 const app = express()
 
 app.use('/', express.static(__dirname + "/static"))
+app.use('/posts', router)
 
 app.listen(4000, () => {
   console.log('server listen on http://localhost:4000')
